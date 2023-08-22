337979f4c414910f536a0d10a1ac15514b2a7e3b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-22 09:26:47 -0300

    implementado futuew para listagem de categoria e conta

--- lib/pages/categorias_page.dart
+++ lib/pages/categorias_page.dart
@@ -1,3 +1,4 @@
+import 'package:expensive_tracker/models/categoria.dart';
 import 'package:expensive_tracker/repository/categoria_repository.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class CategoriasPage extends StatefulWidget {
 }
 
 class _CategoriasPageState extends State<CategoriasPage> {
-  final categorias = CategoriaRepository().listarCategorias();
+  final futureCategorias = CategoriaRepository().listarCategorias();
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +20,39 @@ class _CategoriasPageState extends State<CategoriasPage> {
         appBar: AppBar(
           title: const Text('Categorias'),
         ),
-        body: ListView.separated(
-          itemCount: categorias.length,
-          itemBuilder: (context, index) {
-            final categoria = categorias[index];
-            return CategoriaItem(categoria: categoria);
-          },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
-        ));
+        body: FutureBuilder<List<Categoria>>(
+            future: futureCategorias,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // Future incompleta
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                // Future completa com erro
+                return const Center(
+                  child: Text("Erro ao listas as categorias"),
+                );
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                // Future completa com sucesso
+                return const Center(
+                  child: Text("Nenhuma categoria cadastrada"),
+                );
+              } else {
+                // Future completa com sucesso, com dados
+                final categorias = snapshot.data!;
+
+                return ListView.separated(
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final categoria = categorias[index];
+                    return CategoriaItem(categoria: categoria);
+                  },
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                );
+              }
+            }));
   }
 }
--- lib/pages/contas_page.dart
+++ lib/pages/contas_page.dart
@@ -1,3 +1,6 @@
+import 'package:expensive_tracker/components/conta_item.dart';
+import 'package:expensive_tracker/models/conta.dart';
+import 'package:expensive_tracker/repository/contas_repository.dart';
 import 'package:flutter/material.dart';
 
 class ContasPage extends StatefulWidget {
@@ -8,10 +11,41 @@ class ContasPage extends StatefulWidget {
 }
 
 class _ContasPageState extends State<ContasPage> {
+  final futureContas = ContasRepository().listarContas();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Contas Page'),
-    );
+    return FutureBuilder<List<Conta>>(
+        future: futureContas,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Future incompleta
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            // Future completa com erro
+            return const Center(
+              child: Text("Erro ao listas as contas"),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            // Future completa com sucesso
+            return const Center(
+              child: Text("Nenhuma conta cadastrada"),
+            );
+          } else {
+            // Future completa com sucesso, com dados
+            final contas = snapshot.data!;
+
+            return ListView.separated(
+              itemCount: contas.length,
+              itemBuilder: (context, index) {
+                final conta = contas[index];
+                return ContaItem(conta: conta);
+              },
+              separatorBuilder: (context, index) => const Divider(),
+            );
+          }
+        });
   }
 }
